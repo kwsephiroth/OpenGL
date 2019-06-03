6167c01e262313e60d0c4751177224f41ca80bd1
Kenneth Walker <kwalkerdev@gmail.com>
2019-06-03 12:13:40 -0700

    Added new dependencies and support for Win32 libraries.

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -1,5 +1,10 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <SOIL2.h>
 #include <iostream>
 
 int main(void)
