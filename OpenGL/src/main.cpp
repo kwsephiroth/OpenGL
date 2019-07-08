2160755e919b74aa4cca88ac4f86e75bca49fc80
Kenneth Walker <kwalkerdev@gmail.com>
2019-07-08 08:30:47 -0700

    Removed some unused code.

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -10,18 +10,11 @@
 #include "../Renderer.h"
 #include <iostream>
 
-#define numVAOs 1
-#define numVBOs 1
-#define numIBOs 1
 
 #define ASSERT(x) if (!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
 	x;\
 	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
-
-unsigned int vao[numVAOs];
-unsigned int vbo[numVBOs];
-unsigned int ibo[numIBOs];
 
 unsigned int renderingProgram;
 
@@ -63,56 +56,6 @@ static void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
 	aspect = (float)newWidth / (float)newHeight;
 	glViewport(0, 0, newWidth, newHeight);
 	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
-}
-
-static void setup_vertices()
-{
-	float positions[] = {
-		-0.5f, -0.5f,
-		 0.5f, -0.5f,
-		 0.5f, 0.5f,
-		-0.5f, 0.5f
-	};
-
-	unsigned int indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-
-	GLCall(glGenVertexArrays(numVAOs, vao));
-	GLCall(glBindVertexArray(vao[0]));
-
-	GLCall(glGenBuffers(numVBOs, vbo));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo[0]));
-	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
-	
-	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
-	GLCall(glEnableVertexAttribArray(0));
-
-	GLCall(glGenBuffers(numIBOs, ibo));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo[0]));
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2 * 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
-
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));//Unbinds any buffer object previously bound (Probably not necessary here)
-	GLCall(glBindVertexArray(0)); //Break the existing vertex array object binding after use (Probably not necessary here)
-}
-
-static void draw(GLFWwindow* window, double currentTime)
-{
-	GLCall(glUseProgram(renderingProgram));
-
-	GLCall(glBindVertexArray(vao[0]));
-	//glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
-	//glEnableVertexAttribArray(0);
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo[0]));
-	GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-
-	//glBindBuffer(GL_ARRAY_BUFFER, 0);
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));//Unbinds any buffer object previously bound (Probably not necessary here)
-	GLCall(glBindVertexArray(0)); //Break the existing vertex array object binding after use (Probably not necessary here)
-	GLCall(glUseProgram(0));
 }
 
 int main(void)
@@ -194,11 +137,6 @@ int main(void)
 		glfwPollEvents();
 	}
 	
-	//Clean up
-	//glDeleteVertexArrays(numVAOs, vao);
-	//glDeleteBuffers(numVBOs, vbo);
-	//glDeleteBuffers(numIBOs, ibo);
-
 	glfwTerminate();
 	return 0;
 }
